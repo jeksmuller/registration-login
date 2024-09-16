b6d5c4cb0695f5bd9f0620fa4f193a1ae663ad46
Jacob Delanyo Dotsey <dotseydelanyo11@gmail.com>
2024-09-16 11:44:10 +0000

    added JavaScript

---
+++ scripts/register.js
@@ -0,0 +1,10 @@
+// select form by id
+const registerForm = document.getElementById('register-form');
+// Handle form submit event
+registerForm.onsubmit = function() {
+// Collect Username
+// Collect email
+// Collect password
+// Save user information
+// Send confirmation email
+}
